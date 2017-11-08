93b9ffb1839457cc4dbfc0b9db231762d725572f
lkho <llhmtc@gmail.com>
2017-11-09 00:35:30 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="app">
+    <img src="@/assets/logo.png">
+	<img :src="require('@/assets/logo.png')">
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data () {
+    return {
+    }
+  }
+}
+</script>
