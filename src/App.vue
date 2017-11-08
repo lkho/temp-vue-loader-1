ab9cb38b0de09b7a77b36d706ab9954d439f424b
lkho <llhmtc@gmail.com>
2017-11-09 00:42:22 +0800

    add example

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,8 @@
 <template>
   <div id="app">
-    <img src="@/assets/logo.png">
-	<img :src="require('@/assets/logo.png')">
+    1<img src="@/assets/logo.png"><br/>
+	2<img src="./assets/logo.png"><br/>
+	3<img :src="require('@/assets/logo.png')">
   </div>
 </template>
 
